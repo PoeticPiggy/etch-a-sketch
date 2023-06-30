f655699aa7f3ef4d24673a321bfeb0db4e42c7ff
PoeticPiggy <edward.e.rowlands@gmail.com>
2023-06-30 23:59:58 +0100

    added mouseover event listener

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let gridSize = 16;
 let allSquares = gridSize * gridSize;
+//Size of every box
+let boxSize = 10;
+let containerWidth = gridSize * boxSize + 'px';
 
 const container = document.getElementById("container");
 for (var i = 0; i < allSquares; i++) {
    container.innerHTML += '<div class="box"></div>';
 }
+let boxes = document.querySelectorAll('.box')
+
+boxes.forEach((box) => {
+   box.addEventListener('mouseover', () => {
+      box.style.backgroundColor = 'black'
+    });
+});
+
+container.style.maxWidth = containerWidth;
