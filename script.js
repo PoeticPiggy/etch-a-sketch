5c228cd4d3b7ab0ffafd44adf77c81cb3ea755a8
PoeticPiggy <edward.e.rowlands@gmail.com>
2023-07-01 13:23:23 +0100

    changed box creation from inerHTML to createElement

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let gridSize = prompt('Enter grid size');
+//Changes grid size to number
 +gridSize;
+//Calculates amount of squares needed
 let allSquares = gridSize * gridSize;
 //Size of every box
 let boxSize = 10;
+//Calculates container width
 let containerWidth = gridSize * boxSize + 'px';
 
+//For each square, add a div to container with a class of box
 const container = document.getElementById("container");
 for (var i = 0; i < allSquares; i++) {
-   container.innerHTML += '<div class="box"></div>';
+   div = document.createElement('div');
+   div.classList.add('box');
+   container.appendChild(div);
 }
-let boxes = document.querySelectorAll('.box')
+
+let boxes = document.querySelectorAll('.box');
 
 boxes.forEach((box) => {
    box.addEventListener('mouseover', () => {
