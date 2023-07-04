42647028aa35f09d6be97d5526840fb8bc0dc91a
PoeticPiggy <edward.e.rowlands@gmail.com>
2023-07-04 23:07:01 +0100

    added reset button

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const chooseGridSize = document.querySelector('.gridSize')
+const chooseGridSize = document.querySelector('.gridSize');
 const container = document.getElementById("container");
+const resetButton = document.querySelector('.resetButton');
 
 chooseGridSize.addEventListener('click', () => {
    removeGrid();
    createGrid();
 });
 
+resetButton.addEventListener('click', () => {
+   reset();
+});
+
+
 let boxSize = 10;
 
 function createGrid () {
    let gridSize = prompt('Enter grid size');
-   console.log(gridSize);
 
    while (gridSize < 10 || gridSize > 100) {
       gridSize = prompt('Please enter a number between 10 and 100');
@@ -43,6 +48,13 @@ function removeGrid () {
    let boxes = document.querySelectorAll('.box');
    boxes.forEach((box) => {
       box.remove();
+   });
+}
+
+function reset () {
+   let boxes = document.querySelectorAll('.box');
+   boxes.forEach((box) => {
+      box.style.backgroundColor = 'white';
    });
 }
 
